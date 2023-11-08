b316800e28f4bae6e92b0de896e1230eedd022d2
ks7150 <144281932+ks7150@users.noreply.github.com>
2023-11-08 09:20:25 -0500

    HW09

--- sketch.js
+++ sketch.js
@@ -1,6 +1,51 @@
+let plank;
+let ball;
+let gravity = 0;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
+  
+  plank = createSprite(width / 2, height - 20, 200, 20);
+  plank.immovable = true;
+  
+  ball = createSprite(width / 2, height / 2, 30, 30);
+ 
+  ball.velocity.x = 2;
+  
+  ball.onMousePressed = () => {
+    ball.velocity.y = -10;
+  };
+
+  
+  ball.bounciness= 2
 }
 
 function draw() {
+  background(220);
+  
+  ball.velocity.y += gravity+0.1;
+ 
+  plank.position.x = mouseX
+ if(plank.positon>height + plank+height){
+  resetScreen();
+ }
+  
+  ball.collide(plank);
+  
+ 
+
+  if (ball.position.y > height + ball.height) {
+    resetScreen();
+  }
+}
+
+function resetScreen() {
+  
+  ball.position.x = width / 2;
+  ball.position.y = height / 2;
+  ball.velocity.y = 0;
+
+  plank.position.x = mouseX;
+  plank.position.y = height - 20;
+  plank.velocity.y = 0;
 }
